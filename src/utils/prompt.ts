2c47f0dac661f7e64e93f6abbd10b936882aceb7
Alscof <132270445+Alscof@users.noreply.github.com>
2023-11-11 20:26:40 +0300

    Update prompt.ts

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,16 +1,16 @@
 export const searchQueryPrompt = (task: string, dependent_task: string) => {
-  return `You are an AI assistant tasked with generating a Google search query based on the following task: ${task}. 
+  return `You are an AI assistant tasked with text completion based on the following task: ${task}. 
   ${
     dependent_task.length > 0
       ? `Consider the results of dependent tasks: ${dependent_task}.`
       : ''
   }
   If the task looks like a search query, return the identical search query as your response.
-  Search Query:`;
+  use text completion:`;
 };
 
 export const analystPrompt = (results: string, language: string) => {
-  return `You are an expert analyst. Rewrite the following information as one report without removing any facts.
+  return `You are an expert analyst. Use the following information to write well-researched, detailed, and explained chapter to be part of an APA style research paper without removing any facts.
   Report must be answered in ${language}.
   \n###INFORMATION:${results}.\n###REPORT:`;
 };
@@ -21,7 +21,7 @@ export const textCompletionToolPrompt = (
   task: string,
   dependentTaskOutput: string,
 ) => {
-  let prompt = `Complete your assigned task based on the objective and only based on information provided in the dependent task output, if provided.
+  let prompt = `You are a university professor, Complete your assigned task by writing well-researched, detailed, and explained chapter to be part of an APA style research paper based on the objective and only based on information provided in the dependent task output, if provided. for every topic or task, create 5 to 12 detail and well explained points with Parenthetical citations. Avoid using repeative phrases.
    Your objective: ${objective}. Your task: ${task}
    Output must be answered in ${language}.\n
    `;
