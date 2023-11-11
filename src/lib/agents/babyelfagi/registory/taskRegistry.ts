6423a4aaf9e260b31cc3dfc8607775b5a26b5e70
Alscof <132270445+Alscof@users.noreply.github.com>
2023-11-11 21:33:36 +0300

    Update taskRegistry.ts

--- src/lib/agents/babyelfagi/registory/taskRegistry.ts
+++ src/lib/agents/babyelfagi/registory/taskRegistry.ts
@@ -31,7 +31,7 @@ export class TaskRegistry {
     id: string,
     objective: string,
     skillDescriptions: string,
-    modelName: string = 'gpt-3.5-turbo',
+    modelName: string = 'gpt-4-1106-preview',
     handleMessage: (message: AgentMessage) => Promise<void>,
   ): Promise<void> {
     const relevantObjective = await findMostRelevantObjective(
